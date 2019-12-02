feab4d8f29a3461775568bac310c2773fe24e2c0
Jonathan Norman <jnorman2123@gmail.com>
2019-12-02 15:22:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,20 +3,21 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name = params[:name]
-    "#{@name.reverse}"
+    @reverse_name = params[:name].reverse
+    "#{@reverse_name}"
   end
 
   get '/square/:number' do
-    @num = params[:number].to_i ** 2
-    "#{@num.to_s}"
+    @square_number = params[:number].to_i ** 2
+    "#{@square_number}"
   end
 
   get '/say/:number/:phrase' do
-    @phrase = []
-    @num = params[:number].to_i
-    @num.times {@phrase << params[:phrase]}
-    "#{@phrase.join(", ")}"
+    @phrases = []
+    params[:number].to_i.times do
+      @phrases << params[:phrase]
+    end
+    "#{@phrases.join(", ")}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -26,16 +27,19 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    @operation = params[:operation]
-    case @operation
+    case params[:operation]
     when "add"
-      "#{@num1 + @num2}.to_s"
+      @add = @num1 + @num2
+      "#{@add}"
     when "subtract"
-      "#{@num1 - @num2}.to_s"
+      @subtract = @num1 - @num2
+      "#{@subtract}"
     when "multiply"
-      "#{@num1 * @num2}.to_s"
+      @multiply = @num1 * @num2
+      "#{@multiply}"
     when "divide"
-      "#{@num1 / @num2}.to_s"
+      @divide = @num1 / @num2
+      "#{@divide}"
     end
   end
 end
